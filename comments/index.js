9992dfb34d5b94af8c45abec2eb2ed9ec0f71fc1
niksankarkee <karkeeniksan@gmail.com>
2021-01-18 22:44:40 +0900

    What the hell did i just did

--- comments/index.js
+++ comments/index.js
@@ -11,35 +11,56 @@ app.use(cors());
 const commentsByPostId = {};
 
 app.get('/posts/:id/comments', (req, res) => {
-  // if (commentsByPostId[req.params.id] === undefined) {
-  //   return res.send([]);
-  // }
-  res.send(commentsByPostId[req.params.id]) || [];
+  res.send(commentsByPostId[req.params.id] || []);
 });
 
 app.post('/posts/:id/comments', async (req, res) => {
   const commentId = randomBytes(4).toString('hex');
   const { content } = req.body;
+
   const comments = commentsByPostId[req.params.id] || [];
 
-  comments.push({ id: commentId, content });
+  comments.push({ id: commentId, content, status: 'pending' });
 
   commentsByPostId[req.params.id] = comments;
 
   await axios.post('http://localhost:4005/events', {
-    type: 'commentCreated',
+    type: 'CommentCreated',
     data: {
       id: commentId,
       content,
       postId: req.params.id,
-    },
+      status: 'pending'
+    }
   });
 
   res.status(201).send(comments);
 });
 
-app.post('/events', (req, res) => {
-  console.log('Recived event', req.body.type);
+app.post('/events', async (req, res) => {
+  console.log('Event Received:', req.body.type);
+
+  const { type, data } = req.body;
+
+  if (type === 'CommentModerated') {
+    const { postId, id, status, content } = data;
+    const comments = commentsByPostId[postId];
+
+    const comment = comments.find(comment => {
+      return comment.id === id;
+    });
+    comment.status = status;
+
+    await axios.post('http://localhost:4005/events', {
+      type: 'CommentUpdated',
+      data: {
+        id,
+        status,
+        postId,
+        content
+      }
+    });
+  }
 
   res.send({});
 });
--- posts/index.js
+++ posts/index.js
@@ -20,22 +20,22 @@ app.post('/posts', async (req, res) => {
 
   posts[id] = {
     id,
-    title,
+    title
   };
 
   await axios.post('http://localhost:4005/events', {
-    type: 'postCreated',
+    type: 'PostCreated',
     data: {
       id,
-      title,
-    },
+      title
+    }
   });
 
   res.status(201).send(posts[id]);
 });
 
 app.post('/events', (req, res) => {
-  console.log('Recived event', req.body.type);
+  console.log('Received Event', req.body.type);
 
   res.send({});
 });
