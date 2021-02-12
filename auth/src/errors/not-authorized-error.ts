e552ff06e8fef8f95a6563acacb550f800f79959
Niksan <k-niksan@net-e.co.jp>
2021-02-12 11:31:53 +0900

    auth handling completed

---
+++ auth/src/errors/not-authorized-error.ts
@@ -0,0 +1,14 @@
+﻿import { CustomError } from './custom-error';
+
+export class NotAuthorizedError extends CustomError {
+  statusCode = 401;
+
+  constructor() {
+    super('Not authorized');
+    Object.setPrototypeOf(this, NotAuthorizedError.prototype);
+  }
+
+  serializeErrors() {
+    return [{ message: 'Not authorized' }];
+  }
+}
