894d950e0dce048efb5ddcdb5339817ebf91371c
Niksan <k-niksan@net-e.co.jp>
2021-02-12 15:00:06 +0900

    test with jest completed

---
+++ auth/src/routes/__test__/signin.test.ts
@@ -0,0 +1,33 @@
+﻿import request from 'supertest';
+import { app } from '../../app';
+
+it('failes when a email that does not exist is suppliexd', async () => {
+  await request(app)
+    .post('/api/users/signin')
+    .send({ email: 'test@test.com', password: '123456' })
+    .expect(400);
+});
+
+it('fails when an incorrect password is supplied', async () => {
+  await request(app)
+    .post('/api/users/signup')
+    .send({ email: 'test@test.com', password: '123456' })
+    .expect(201);
+  await request(app)
+    .post('/api/users/signin')
+    .send({ email: 'test@test.com', password: 'asdfasdf' })
+    .expect(400);
+});
+
+it('response with a cookie when given valid creendentials', async () => {
+  await request(app)
+    .post('/api/users/signup')
+    .send({ email: 'test@test.com', password: '123456' })
+    .expect(201);
+  const response = await request(app)
+    .post('/api/users/signin')
+    .send({ email: 'test@test.com', password: '123456' })
+    .expect(200);
+
+  expect(response.get('Set-Cookie')).toBeDefined();
+});
---
+++ auth/src/routes/__test__/signout.test.ts
@@ -0,0 +1,16 @@
+﻿import request from 'supertest';
+import { app } from '../../app';
+
+it('clears the cookie after signing out', async () => {
+  await request(app)
+    .post('/api/users/signup')
+    .send({ email: 'test@test.com', password: '123456' })
+    .expect(201);
+
+  const response = await request(app)
+    .post('/api/users/signout')
+    .send({})
+    .expect(200);
+
+  //   console.log(response.get('Set-Cookie'));
+});
