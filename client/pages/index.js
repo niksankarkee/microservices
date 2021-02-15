d4f30c9a4582c0a6ea9682693f503ff9b87d430b
niksankarkee <karkeeniksan@gmail.com>
2021-02-15 10:30:45 +0900

    install package and work for client

--- client/pages/index.js
+++ client/pages/index.js
@@ -1,3 +1,13 @@
-export default () => {
+import axios from 'axios';
+
+const LandignPage = ({ currentUser }) => {
+  console.log(currentUser);
   return <h1>Landing page</h1>;
 };
+
+LandignPage.getInitialProps = async () => {
+  const response = await axios.get('/api/users/currentuser');
+  return response.data;
+};
+
+export default LandignPage;
