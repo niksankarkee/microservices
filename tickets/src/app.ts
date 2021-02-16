16a30c521e5a8ac8534b91105c5fb0512e8122a4
Niksan <k-niksan@net-e.co.jp>
2021-02-16 14:50:54 +0900

    created a ticket pararelly testing

--- tickets/src/app.ts
+++ tickets/src/app.ts
@@ -2,7 +2,8 @@
 import 'express-async-errors';
 import { json } from 'body-parser';
 import cookieSession from 'cookie-session';
-import { errorHandler, NotFoundError } from '@nksgtickets/common';
+import { errorHandler, NotFoundError, currentUser } from '@nksgtickets/common';
+import { createTicketRouter } from './routes/new';
 
 const app = express();
 app.set('trust proxy', true);
@@ -10,6 +11,9 @@ app.use(json());
 app.use(
   cookieSession({ signed: false, secure: process.env.NODE_ENV !== 'test' })
 );
+
+app.use(currentUser);
+app.use(createTicketRouter);
 
 app.all('*', async (req, res) => {
   throw new NotFoundError();
--- tickets/src/test/setup.ts
+++ tickets/src/test/setup.ts
@@ -2,11 +2,12 @@
 import mongoose from 'mongoose';
 import request from 'supertest';
 import { app } from '../app';
+import jwt from 'jsonwebtoken';
 
 declare global {
   namespace NodeJS {
     interface Global {
-      signin(): Promise<string[]>;
+      signin(): string[];
     }
   }
 }
@@ -37,15 +38,25 @@ afterAll(async () => {
   await mongoose.connection.close();
 });
 
-global.signin = async () => {
-  const email = 'test@test.com';
-  const password = '123456';
+global.signin = () => {
+  // Build a JWT payload. {id, email}
+  const payload = {
+    id: 'asdfjklas',
+    email: 'test@test.com',
+  };
 
-  const response = await request(app)
-    .post('/api/users/signup')
-    .send({ email, password })
-    .expect(201);
+  // Create the JWT!
+  const token = jwt.sign(payload, process.env.JWT_KEY!);
 
-  const cookie = response.get('Set-Cookie');
-  return cookie;
+  // Build session Object. {jwt: MY_JWT}
+  const session = { jwt: token };
+
+  // Turn that session into JSON
+  const sessionJson = JSON.stringify(session);
+
+  // Take JSON and encode it as base64
+  const base64 = Buffer.from(sessionJson).toString('base64');
+
+  // return a string thats the cookie with the encoded data
+  return [`express:sess=${base64}`];
 };
