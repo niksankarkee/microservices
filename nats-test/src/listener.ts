c1cd787d8c20f194076f972f286e37e43955fe44
niksankarkee <karkeeniksan@gmail.com>
2021-02-18 21:44:05 +0900

    nats publish & listen

--- nats-test/src/listener.ts
+++ nats-test/src/listener.ts
@@ -1,1 +1,17 @@
-﻿
+﻿import nats, { Message } from 'node-nats-streaming';
+console.clear();
+const stan = nats.connect('ticketing', '123', {
+  url: 'http://localhost:4222',
+});
+
+stan.on('connect', () => {
+  console.log('Listener connected to nat');
+  const subscription = stan.subscribe('ticket:created');
+  subscription.on('message', (msg: Message) => {
+    const data = msg.getData();
+
+    if (typeof data === 'string') {
+      console.log(`Reciived  event #${msg.getSequence()}, with data: ${data}`);
+    }
+  });
+});
