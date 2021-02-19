a6fb4d5604ad34d9828d4a752e01ed19cab4860d
Niksan <k-niksan@net-e.co.jp>
2021-02-19 11:00:58 +0900

    ack mode and off default behevour

--- nats-test/src/listener.ts
+++ nats-test/src/listener.ts
@@ -1,17 +1,26 @@
 ﻿import nats, { Message } from 'node-nats-streaming';
+import { randomBytes } from 'crypto';
 console.clear();
-const stan = nats.connect('ticketing', '123', {
+const stan = nats.connect('ticketing', randomBytes(4).toString('hex'), {
   url: 'http://localhost:4222',
 });
 
 stan.on('connect', () => {
   console.log('Listener connected to nat');
-  const subscription = stan.subscribe('ticket:created');
+
+  const options = stan.subscriptionOptions().setManualAckMode(true);
+
+  const subscription = stan.subscribe(
+    'ticket:created',
+    'orders-service-queur-group',
+    options
+  );
   subscription.on('message', (msg: Message) => {
     const data = msg.getData();
 
     if (typeof data === 'string') {
       console.log(`Reciived  event #${msg.getSequence()}, with data: ${data}`);
     }
+    msg.ack();
   });
 });
