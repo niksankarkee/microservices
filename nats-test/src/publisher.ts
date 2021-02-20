b26649094147167642b9cd1c28c0222a94520241
niksankarkee <karkeeniksan@gmail.com>
2021-02-20 11:02:06 +0900

    before moving publisher & listener inside common module

--- nats-test/src/publisher.ts
+++ nats-test/src/publisher.ts
@@ -1,19 +1,30 @@
 ﻿import nats from 'node-nats-streaming';
+import { TicketCreatedPublisher } from './events/ticket-created-publisher';
 console.clear();
 const stan = nats.connect('ticketing', 'abc', {
   url: 'http://localhost:4222',
 });
 
-stan.on('connect', () => {
+stan.on('connect', async () => {
   console.log('Publisher connected to NATS');
 
-  const data = JSON.stringify({
-    id: '123',
-    title: 'concert',
-    price: 20,
-  });
+  const publisher = new TicketCreatedPublisher(stan);
+  try {
+    await publisher.publish({
+      id: '123',
+      title: 'concert',
+      price: 20,
+    });
+  } catch (error) {
+    console.error(error);
+  }
+  // const data = JSON.stringify({
+  //   id: '123',
+  //   title: 'concert',
+  //   price: 20,
+  // });
 
-  stan.publish('ticket:created', data, () => {
-    console.log('Event published');
-  });
+  // stan.publish('ticket:created', data, () => {
+  //   console.log('Event published');
+  // });
 });
